d6153d22cbf8c81e985bae9e96ea356361e7242b
sunkh964 <sunkh964@gmail.com>
2024-08-21 11:30:50 +0900

    8/21일 시작

---
+++ src/test/java/com/green/Team3/Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.Team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
