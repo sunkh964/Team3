a648cf3f2ead3ed5a96f7b0bfa61704fc4f5d0ed
nohk1113 <nohk1113@naver.com>
2024-08-22 15:23:21 +0900

    완료

--- src/main/java/com/green/Team3/res/vo/ResVO.java
+++ src/main/java/com/green/Team3/res/vo/ResVO.java
@@ -1,4 +1,12 @@
 package com.green.Team3.res.vo;
 
+import lombok.Data;
+
+@Data
 public class ResVO {
+    private int resNum;
+    private int memId;
+    private String resTime;
+    private int staffNum;
+    private int partNum;
 }
