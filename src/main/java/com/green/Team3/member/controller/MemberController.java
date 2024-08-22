de2ea4779c2c77c6420895704dd829a0d689a990
da9630jj <da9630jj@gmail.com>
2024-08-22 15:23:55 +0900

    dayoung

--- src/main/java/com/green/Team3/member/controller/MemberController.java
+++ src/main/java/com/green/Team3/member/controller/MemberController.java
@@ -1,4 +1,14 @@
 package com.green.Team3.member.controller;
 
+import com.green.Team3.member.service.MemberService;
+import com.green.Team3.member.service.MemberServiceImpl;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
 public class MemberController {
+    @Resource(name = "memberService")
+    private MemberService memberService;
 }
--- src/main/java/com/green/Team3/staff/service/StaffServiceImpl.java
+++ src/main/java/com/green/Team3/staff/service/StaffServiceImpl.java
@@ -1,4 +1,11 @@
 package com.green.Team3.staff.service;
 
-public class StaffServiceImpl {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("staffService")
+public class StaffServiceImpl implements StaffService {
+    @Autowired
+    private SqlSessionTemplate sqlSession;
 }
