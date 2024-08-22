bd61a526370633d9508b7cbf86ba2d55a1b847b7
nubbp <nubi0126@gmail.com>
2024-08-22 15:49:18 +0900

    mapper3

--- src/main/java/com/green/Team3/history/vo/HistoryVO.java
+++ src/main/java/com/green/Team3/history/vo/HistoryVO.java
@@ -2,6 +2,8 @@ package com.green.Team3.history.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class HistoryVO {
     private int hisNum;
@@ -9,4 +11,5 @@ public class HistoryVO {
     private String illName;
     private String illDetail;
     private int staffNum;
+    private List<HistoryVO> staffList;
 }
