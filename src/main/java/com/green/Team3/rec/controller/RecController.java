7b2a91d150ce5e563075102d7d74c62463452f08
da9630jj <da9630jj@gmail.com>
2024-09-04 11:07:36 +0900

    ldjifejd

--- src/main/java/com/green/Team3/rec/controller/RecController.java
+++ src/main/java/com/green/Team3/rec/controller/RecController.java
@@ -1,9 +1,14 @@
 package com.green.Team3.rec.controller;
 
+import com.green.Team3.patie.service.PatieService;
+import com.green.Team3.rec.service.RecService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rec")
 public class RecController {
+    @Resource(name = "recService")
+    private RecService recService;
 }
