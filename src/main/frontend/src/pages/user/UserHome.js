f54271f51e13e4ce1128ec6f53d3a7ca6dc09f55
sunkh964 <sunkh964@gmail.com>
2024-09-04 16:03:46 +0900

    240904

--- src/main/frontend/src/pages/user/UserHome.js
+++ src/main/frontend/src/pages/user/UserHome.js
@@ -27,6 +27,11 @@ const UserHome = () => {
 const [showImg, setShowImage] = useState(0);
 
 const img = [
+   'http://localhost:8080/images/img_introRolling02.jpg',
+   'http://localhost:8080/images/FIL_20240719140842069Pf20.jpg',
+   'http://localhost:8080/images/img_introRolling02.jpg'
+]
+const img2 = [
    'http://localhost:8080/images/hos_banner_waifu2x_art_noise1_scale.png',
    'http://localhost:8080/images/FIL_20240719140842069Pf20.jpg',
    'http://localhost:8080/images/hos_banner_waifu2x_art_noise1_scale.png'
@@ -49,7 +54,7 @@ const img = [
    <div className='home-div'>
       <div className='img'> 
          <img src={img[showImg]} />
-         <img className='img2' src={img[showImg]} />
+         <img className='img2' src={img2[showImg]} />
          <div>
             <ul className='img-list'>
                {
